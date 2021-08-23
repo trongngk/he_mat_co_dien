41564152e548ff47eeaaeae186b2e54173c20207
trongngk <53163510+trongngk@users.noreply.github.com>
2021-08-23 07:52:28 +0700

    Create shift_cipher.py

---
+++ shift_cipher.py
@@ -0,0 +1,68 @@
+import string
+# mã hóa
+plantext = input("Input plantext: ")
+key = int(input("Input key: "))
+cipher = ""
+print("Symbol", end = " | ")
+P_index = []
+E_x = []
+for symbol in plantext:
+    print("{:<3}".format(symbol), end = "|")
+    if symbol.isupper():
+        index = string.ascii_uppercase.find(symbol)
+        P_index.append(index)
+        index = (index + key) % 26
+        E_x.append(index)
+        cipher += string.ascii_uppercase[index]
+    elif symbol.islower():
+        index = string.ascii_lowercase.find(symbol)
+        P_index.append(index)
+        index = (index + key) % 26
+        E_x.append(index)
+        cipher += string.ascii_lowercase[index]
+    else:
+        P_index.append(symbol)
+        E_x.append(symbol)
+        cipher += symbol
+print("\nIndex ", end = " | ")
+for i in P_index:
+    print("{:<3}".format(i),end = "|")
+print("\nE(x)  ",end = " | ")
+for j in E_x:
+    print("{:<3}".format(j), end = "|")
+print("\nCipher", end = " | ")
+for c in cipher:
+    print("{:<3}".format(c), end = "|")
+print("\n____\n____")
+# giải mã
+decode = ""
+print("Symbol", end = " | ")
+C_index = []
+E_y = []
+for symbol in cipher:
+    print("{:<3}".format(symbol), end = "|")
+    if symbol.isupper():
+        index = string.ascii_uppercase.find(symbol)
+        C_index.append(index)
+        index = (index - key) % 26
+        E_y.append(index)
+        decode += string.ascii_uppercase[index]
+    elif symbol.islower():
+        index = string.ascii_lowercase.find(symbol)
+        C_index.append(index)
+        index = (index - key) % 26
+        E_y.append(index)
+        decode  += string.ascii_lowercase[index]
+    else:
+        C_index.append(symbol)
+        E_y.append(symbol)
+        decode  += symbol
+print("\nIndex ", end = " | ")
+for i in C_index:
+    print("{:<3}".format(i),end = "|")
+print("\nE(x)  ",end = " | ")
+for j in E_y:
+    print("{:<3}".format(j), end = "|")
+print("\nCipher", end = " | ")
+for c in decode:
+    print("{:<3}".format(c), end = "|")
